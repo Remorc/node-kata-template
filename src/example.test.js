4814a7d5986e28f80cc4f445dae7a302320ad3f5
Brandon Cromer <bcromer07@gmail.com>
2023-09-28 10:59:21 -0400

    Added test

--- src/example.test.js
+++ src/example.test.js
@@ -2,8 +2,14 @@ const example = require('./example')
 
 describe("sum", () => {
     it("adds two numbers", () => {
-        const addedNumbers = example.sum(1, 2)
+        let addedNumbers = example.sum(1, 2)
 
         expect(addedNumbers).toEqual(3)
+    })
+    
+    it("handles zero", () => {
+        let addedNumbers = example.sum(1, 0)
+
+        expect(addedNumbers).toEqual(1)
     })
 })
