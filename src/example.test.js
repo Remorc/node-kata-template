100ef0932a4de67c260e2b291f91d647d21b0e38
Brandon Cromer <bcromer07@gmail.com>
2023-10-17 20:28:45 -0400

    Added another function type

--- src/example.test.js
+++ src/example.test.js
@@ -1,6 +1,14 @@
 const example = require('./example')
 
+function addNegativeNumber() {
+    let addedNumbers = example.sum(-1, 2)
+
+    expect(addedNumbers).toEqual(1)
+}
+
 describe("sum", () => {
+    it("adds negative numbers", addNegativeNumber)   // This is using a named function
+
     it("adds two numbers", function () {      // This is using an anonymous unnamed function, which is just a shorthand
         let addedNumbers = example.sum(1, 2)
 
