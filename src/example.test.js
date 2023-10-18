49684445a7c7e135613a311376abae02e558b75c
Brandon Cromer <bcromer07@gmail.com>
2023-10-17 20:31:41 -0400

    Adjusted comments

--- src/example.test.js
+++ src/example.test.js
@@ -9,13 +9,13 @@ function addNegativeNumber() {
 describe("sum", () => {
     it("adds negative numbers", addNegativeNumber)   // This is using a named function
 
-    it("adds two numbers", function () {      // This is using an anonymous unnamed function, which is just a shorthand
+    it("adds two numbers", function () {      // This is using an anonymous unnamed function
         let addedNumbers = example.sum(1, 2)
 
         expect(addedNumbers).toEqual(3)
     })
 
-    it("handles zero", () => {                // This is using an anonymous arrow function, which is just a shorthand
+    it("handles zero", () => {                // This is using an anonymous arrow function, which is just a shorthand for an anonymous unnamed function
         let addedNumbers = example.sum(1, 0)
 
         expect(addedNumbers).toEqual(1)
