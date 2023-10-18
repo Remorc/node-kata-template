2ffc8f21485271aa809baad2cd80b3e96d6ca473
Brandon Cromer <bcromer07@gmail.com>
2023-10-17 20:14:06 -0400

    Added two different function types

--- src/example.test.js
+++ src/example.test.js
@@ -1,13 +1,13 @@
 const example = require('./example')
 
 describe("sum", () => {
-    it("adds two numbers", () => {
+    it("adds two numbers", function () {      // This is using an anonymous unnamed function, which is just a shorthand
         let addedNumbers = example.sum(1, 2)
 
         expect(addedNumbers).toEqual(3)
     })
-    
-    it("handles zero", () => {
+
+    it("handles zero", () => {                // This is using an anonymous arrow function, which is just a shorthand
         let addedNumbers = example.sum(1, 0)
 
         expect(addedNumbers).toEqual(1)
